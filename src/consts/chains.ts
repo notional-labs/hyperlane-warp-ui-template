65dc34a4ba02383f3c447dded0683dc0a6722213
minhngoc270499 <ngocduongminh.work@gmail.com>
2024-05-28 08:33:56 +0900

    feat: bob mainnet

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,31 +1,31 @@
-import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
+import {ChainMap, ChainMetadata, ExplorerFamily} from '@hyperlane-xyz/sdk';
+import {ProtocolType} from "@hyperlane-xyz/utils";
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
 // Chains already in the SDK need not be included here unless you want to override some fields
 // Schema here: https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/metadata/chainMetadataTypes.ts
 export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
-  // mycustomchain: {
-  //   protocol: ProtocolType.Ethereum,
-  //   chainId: 123123,
-  //   domainId: 123123,
-  //   name: 'mycustomchain',
-  //   displayName: 'My Chain',
-  //   nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  //   rpcUrls: [{ http: 'https://mycustomchain-rpc.com' }],
-  //   blockExplorers: [
-  //     {
-  //       name: 'MyCustomScan',
-  //       url: 'https://mycustomchain-scan.com',
-  //       apiUrl: 'https://api.mycustomchain-scan.com/api',
-  //       family: ExplorerFamily.Etherscan,
-  //     },
-  //   ],
-  //   blocks: {
-  //     confirmations: 1,
-  //     reorgPeriod: 1,
-  //     estimateBlockTime: 10,
-  //   },
-  //   logoURI: '/logo.svg',
-  // },
+  bob: {
+    protocol: ProtocolType.Ethereum,
+    chainId: 60808,
+    domainId: 60808,
+    name: 'bob',
+    displayName: 'BOB',
+    nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    rpcUrls: [{ http: 'https://rpc.gobob.xyz/' }],
+    blockExplorers: [
+      {
+        name: 'BOB explorer',
+        url: 'https://explorer.gobob.xyz/',
+        apiUrl: 'https://explorer.gobob.xyz/api',
+        family: ExplorerFamily.Blockscout,
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 1,
+      estimateBlockTime: 10,
+    },
+  },
 };
